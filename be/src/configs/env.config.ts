e60c78838c60ef5b110a1baf09cbb1c8ac7326da
Linh_ASUS <vanlinh042004@gmail.com>
2025-06-13 11:58:51 +0700

    Change URL Mongo Atlas

--- be/src/configs/env.config.ts
+++ be/src/configs/env.config.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  MONGODB_URL: "mongodb://127.0.0.1:27017/examdb",
+  //MONGODB_URL: "mongodb://127.0.0.1:27017/examdb",
+  MONGODB_URL:
+    "mongodb+srv://vanlinh042004:rmzl5RaH8WkSah31@cluster0.5cvqe9s.mongodb.net/examdb?retryWrites=true&w=majority&appName=Cluster0",
   PORT: 5000,
 };
